ee150a65e27778ba0c52fa883d647992c534488d
Andres6936 <joan.buritica@artemisa.tech>
2022-11-19 12:06:22 -0500

    Added New Panel

--- source/interfaz/panelOpciones.py
+++ source/interfaz/panelOpciones.py
@@ -4,17 +4,32 @@ class PanelOpciones(tk.LabelFrame):
     
     def __init__(self, parent):
         
-        tk.LabelFrame.__init__(self, parent, text='Opciones')
+        tk.LabelFrame.__init__(self, parent, text='Connection Mongo')
         
         self.config(background='white')
         
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
+
+        self.usernameLabel = tk.Label(self, text="Username")
+        self.usernameLabel.grid(row=1, column=0, sticky=tk.E + tk.W)
+
+        self.username = tk.StringVar()
+        self.usernameInput = tk.Entry(self, textvariable=self.username)
+        self.usernameInput.grid(row=1, column=1, sticky=tk.E + tk.W)
+
+        self.passwordLabel = tk.Label(self, text="Password")
+        self.passwordLabel.grid(row=1, column=2, sticky=tk.E + tk.W)
+
+        self.password = tk.StringVar()
+        self.passwordInput = tk.Entry(self, textvariable=self.password)
+        self.passwordInput.grid(row=1, column=3, sticky=tk.E + tk.W)
+
+        self.buttonConnect = tk.Button(self, text='Connect', command=self.connect)
+        self.buttonConnect.grid(row=2, column=0, columnspan=2, sticky=tk.E+tk.W)
         
-        self.botonOpcion1 = tk.Button(self, text='Opcion 1')
-        self.botonOpcion1.grid(row=0, column=0, padx=1, pady=2,
-            sticky=tk.E+tk.W)
-        
-        self.botonOpcion2 = tk.Button(self, text='Opcion 2')
-        self.botonOpcion2.grid(row=0, column=1, padx=1, pady=2,
-            sticky=tk.E+tk.W)
+        self.buttonCancel = tk.Button(self, text='Cancel')
+        self.buttonCancel.grid(row=2, column=2, columnspan=2, sticky=tk.E+tk.W)
+
+    def connect(self):
+        print("Connect")
