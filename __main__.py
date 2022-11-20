65207a6c82083717f219c62d3e3315143995602e
Andres6936 <joan.buritica@artemisa.tech>
2022-11-19 20:52:14 -0500

    Added Theme

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
 import tkinter as tk
-# Avoid the blurry font in Windows 10 - Reference: https://stackoverflow.com/a/43046744
 from ctypes import windll
 
 from source.interfaz.ConnectionInterface import ConnectionInterface
 
+# Avoid the blurry font in Windows 10 - Reference: https://stackoverflow.com/a/43046744
 windll.shcore.SetProcessDpiAwareness(1)
 
 
@@ -17,17 +17,23 @@ class Application(tk.Frame):
         # Obtenemos el ventana superior (Top-Level window)
         top = self.winfo_toplevel()
         top.resizable(width=False, height=False)
-        
+
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
 
         self.interfazTriangulo = ConnectionInterface(self)
-        self.interfazTriangulo.grid(row=0, sticky=tk.E+tk.W)
-        
-        self.grid(sticky=tk.N+tk.E+tk.S+tk.W)
+        self.interfazTriangulo.grid(row=0, sticky=tk.E + tk.W)
 
-        
-app = Application()
-app.master.title('N1-Triangulo-Python-Tkinter')
-app.mainloop()
+        self.grid(sticky=tk.N + tk.E + tk.S + tk.W)
 
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.tk.call("source", "azure.tcl")
+    root.tk.call("set_theme", "light")
+    root.title("N1-Triangulo-Python-Tkinter")
+
+    app = Application(root)
+    app.pack(fill="both", expand=True)
+
+    root.mainloop()
