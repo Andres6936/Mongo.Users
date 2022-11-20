f83533e9f2a217bfadda7b26d425f385464e3a2e
Andres6936 <joan.buritica@artemisa.tech>
2022-11-20 11:02:51 -0500

    [General] Added more elements

--- source/interfaz/PanelGeneral.py
+++ source/interfaz/PanelGeneral.py
@@ -6,30 +6,31 @@ class PanelGeneral(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         self.connectionStringScheme = tk.Label(self, text="Connection String Scheme")
-        self.connectionStringScheme.grid()
+        self.connectionStringScheme.grid(row=0, column=0, sticky=tk.W)
 
         self.buttonConnectionMongo = tk.Button(self, text="mongodb")
-        self.buttonConnectionMongo.grid()
+        self.buttonConnectionMongo.grid(row=1, column=0, sticky=tk.W + tk.E)
 
         self.buttonConnectionSRV = tk.Button(self, text="mongodb+svr")
-        self.buttonConnectionSRV.grid()
+        self.buttonConnectionSRV.grid(row=1, column=1, sticky=tk.W + tk.E)
 
         self.buttonDescription = tk.Label(self, text="Standard Connection String Format. "
                                                      "The standard format of the MongoDB "
                                                      "connection URI is used to connect "
                                                      "to a MongoDB deployment: standalone,"
-                                                     " replica set, or a sharded cluster.", wraplength="400px")
-        self.buttonDescription.grid()
+                                                     " replica set, or a sharded cluster.",
+                                          wraplength="400px")
+        self.buttonDescription.grid(row=2, column=0, columnspan=2, sticky=tk.W)
 
         self.labelHostname = tk.Label(self, text="Host")
-        self.labelHostname.grid()
+        self.labelHostname.grid(row=3, column=0, columnspan=2, sticky=tk.W)
 
         self.inputHostname = tk.Entry(self)
-        self.inputHostname.grid()
+        self.inputHostname.grid(row=4, column=0, columnspan=2, sticky=tk.W + tk.E)
 
         self.checkDirectConnection = tk.Checkbutton(self, text="Direct Connection")
-        self.checkDirectConnection.grid()
+        self.checkDirectConnection.grid(row=5, column=0, sticky=tk.W)
 
         self.labelDescriptionDirectConnection = tk.Label(self,
                                                          text="Specifies whether to force dispatch all operations to the specified host.")
-        self.labelDescriptionDirectConnection.grid()
+        self.labelDescriptionDirectConnection.grid(row=6, column=0, sticky=tk.W)
