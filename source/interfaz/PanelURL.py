3bc6ba751426d483b237e5846d74bb5441bb6795
Andres6936 <joan.buritica@artemisa.tech>
2022-11-19 21:50:28 -0500

    Responsive Layout

--- source/interfaz/PanelURL.py
+++ source/interfaz/PanelURL.py
@@ -10,10 +10,10 @@ class PanelURL(tk.Frame):
         self.grid_columnconfigure((0, 1), uniform="uniform", weight=1)
 
         self.labelNewConnection = tk.Label(self, text="New Connection")
-        self.labelNewConnection.grid(row=0, column=0, columnspan=2, sticky=tk.E + tk.W)
+        self.labelNewConnection.grid(row=0, column=0, columnspan=2, sticky=tk.W)
 
         self.labelDescription = tk.Label(self, text="Connect to a MongoDB deployment")
-        self.labelDescription.grid(row=1, column=0, columnspan=2, sticky=tk.E + tk.W)
+        self.labelDescription.grid(row=1, column=0, columnspan=2, sticky=tk.W)
 
         self.labelURI = tk.Label(self, text='URI')
         self.labelURI.grid(row=2, column=0, sticky=tk.E + tk.W)
@@ -25,4 +25,4 @@ class PanelURL(tk.Frame):
         self.entryURI.grid(row=3, column=0, columnspan=2, sticky=tk.E + tk.W)
 
         self.labelAdvanceConnectionOptions = tk.Label(self, text="Advanced Connection Options")
-        self.labelAdvanceConnectionOptions.grid(row=4, column=0, columnspan=2, sticky=tk.E + tk.W)
+        self.labelAdvanceConnectionOptions.grid(row=4, column=0, columnspan=2, sticky=tk.W)
