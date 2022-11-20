688f79715641606ccb016a556ac3c1a350209896
Andres6936 <joan.buritica@artemisa.tech>
2022-11-20 10:54:21 -0500

    [General] Added more elements

--- source/interfaz/PanelGeneral.py
+++ source/interfaz/PanelGeneral.py
@@ -14,8 +14,22 @@ class PanelGeneral(tk.Frame):
         self.buttonConnectionSRV = tk.Button(self, text="mongodb+svr")
         self.buttonConnectionSRV.grid()
 
+        self.buttonDescription = tk.Label(self, text="Standard Connection String Format. "
+                                                     "The standard format of the MongoDB "
+                                                     "connection URI is used to connect "
+                                                     "to a MongoDB deployment: standalone,"
+                                                     " replica set, or a sharded cluster.", wraplength="400px")
+        self.buttonDescription.grid()
+
         self.labelHostname = tk.Label(self, text="Host")
         self.labelHostname.grid()
 
         self.inputHostname = tk.Entry(self)
         self.inputHostname.grid()
+
+        self.checkDirectConnection = tk.Checkbutton(self, text="Direct Connection")
+        self.checkDirectConnection.grid()
+
+        self.labelDescriptionDirectConnection = tk.Label(self,
+                                                         text="Specifies whether to force dispatch all operations to the specified host.")
+        self.labelDescriptionDirectConnection.grid()
