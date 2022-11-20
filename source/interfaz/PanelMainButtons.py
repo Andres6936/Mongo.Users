c8ee1757807f76ecde33b42efbd5e9933f96a7aa
Andres6936 <joan.buritica@artemisa.tech>
2022-11-19 21:44:09 -0500

    Responsive Layout

--- source/interfaz/PanelMainButtons.py
+++ source/interfaz/PanelMainButtons.py
@@ -6,13 +6,13 @@ class PanelMainButtons(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         self.grid(padx=4, pady=4)
-        self.grid_columnconfigure((0, 1, 2), uniform="uniform", weight=1)
+        self.grid_columnconfigure((0, 1, 2, 3, 4), uniform="uniform", weight=1)
 
         self.buttonSave = tk.Button(self, text='Save')
         self.buttonSave.grid(row=0, column=0, sticky=tk.E + tk.W)
 
         self.buttonSaveConnect = tk.Button(self, text='Save Connect')
-        self.buttonSaveConnect.grid(row=0, column=1, sticky=tk.E + tk.W)
+        self.buttonSaveConnect.grid(row=0, column=3, sticky=tk.E + tk.W)
 
         self.buttonConnect = tk.Button(self, text='Connect')
-        self.buttonConnect.grid(row=0, column=2, sticky=tk.E + tk.W)
+        self.buttonConnect.grid(row=0, column=4, sticky=tk.E + tk.W)
