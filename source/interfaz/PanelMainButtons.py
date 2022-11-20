c2fb9a693465bb50a58219f63c3f556f7765892d
Andres6936 <joan.buritica@artemisa.tech>
2022-11-19 21:39:25 -0500

    Added Position

--- source/interfaz/PanelMainButtons.py
+++ source/interfaz/PanelMainButtons.py
@@ -5,8 +5,8 @@ class PanelMainButtons(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
-        self.config(padx=4, pady=4)
-        self.columnconfigure(0, weight=1)
+        self.grid(padx=4, pady=4)
+        self.grid_columnconfigure((0, 1, 2), uniform="uniform", weight=1)
 
         self.buttonSave = tk.Button(self, text='Save')
         self.buttonSave.grid(row=0, column=0, sticky=tk.E + tk.W)
--- source/interfaz/PanelURL.py
+++ source/interfaz/PanelURL.py
@@ -6,13 +6,14 @@ class PanelURL(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
-        self.config(padx=4, pady=4)
+        self.grid(padx=4, pady=4)
+        self.grid_columnconfigure((0, 1), uniform="uniform", weight=1)
 
         self.labelNewConnection = tk.Label(self, text="New Connection")
-        self.labelNewConnection.grid(row=0, column=0, sticky=tk.E + tk.W)
+        self.labelNewConnection.grid(row=0, column=0, columnspan=2, sticky=tk.E + tk.W)
 
         self.labelDescription = tk.Label(self, text="Connect to a MongoDB deployment")
-        self.labelDescription.grid(row=1, column=0, sticky=tk.E + tk.W)
+        self.labelDescription.grid(row=1, column=0, columnspan=2, sticky=tk.E + tk.W)
 
         self.labelURI = tk.Label(self, text='URI')
         self.labelURI.grid(row=2, column=0, sticky=tk.E + tk.W)
@@ -21,7 +22,7 @@ class PanelURL(tk.Frame):
         self.editConnectionString.grid(row=2, column=1, sticky=tk.E + tk.W)
 
         self.entryURI = tk.Entry(self)
-        self.entryURI.grid(row=3, column=0, sticky=tk.E + tk.W)
+        self.entryURI.grid(row=3, column=0, columnspan=2, sticky=tk.E + tk.W)
 
         self.labelAdvanceConnectionOptions = tk.Label(self, text="Advanced Connection Options")
-        self.labelAdvanceConnectionOptions.grid(row=4, column=0, sticky=tk.E + tk.W)
+        self.labelAdvanceConnectionOptions.grid(row=4, column=0, columnspan=2, sticky=tk.E + tk.W)
