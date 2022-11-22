93556e7134409103cc9c4a2fbeb223962ef0cfec
Andres6936 <joan.buritica@artemisa.tech>
2022-11-21 20:13:24 -0500

    New Design

--- source/interfaz/PanelProxySSH.py
+++ source/interfaz/PanelProxySSH.py
@@ -1,17 +1,23 @@
 import tkinter as tk
+from tkinter import ttk
 
 class PanelProxySSH(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
-        self.buttonNone = tk.Button(self, text="None")
-        self.buttonNone.grid()
+        self.grid_columnconfigure((0, 1, 2, 3), uniform="uniform", weight=1)
 
-        self.buttonSSHWithPassword = tk.Button(self, text="SSH with Password")
-        self.buttonSSHWithPassword.grid()
+        self.labelTitle = ttk.Label(self, text="SSH Tunnel/Proxy Method")
+        self.labelTitle.grid(row=0, column=0)
 
-        self.buttonSSHWithIdentityFile = tk.Button(self, text="SSH with Identify File")
-        self.buttonSSHWithIdentityFile.grid()
+        self.buttonNone = ttk.Button(self, text="None")
+        self.buttonNone.grid(row=1, column=0)
 
-        self.buttonSocks5 = tk.Button(self, text="Socks5")
-        self.buttonSocks5.grid()
+        self.buttonSSHWithPassword = ttk.Button(self, text="SSH with Password")
+        self.buttonSSHWithPassword.grid(row=1, column=1)
+
+        self.buttonSSHWithIdentityFile = ttk.Button(self, text="SSH with Identify File")
+        self.buttonSSHWithIdentityFile.grid(row=1, column=2)
+
+        self.buttonSocks5 = ttk.Button(self, text="Socks5")
+        self.buttonSocks5.grid(row=1, column=3)
