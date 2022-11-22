e9b866a45496bd63244a0fb090a265b1b853a3db
Andres6936 <joan.buritica@artemisa.tech>
2022-11-21 21:31:12 -0500

    New Design

--- source/interfaz/PanelAdvanced.py
+++ source/interfaz/PanelAdvanced.py
@@ -1,26 +1,41 @@
 import tkinter as tk
+from tkinter import ttk
 
 class PanelAdvanced(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
+        self.grid_columnconfigure((0, 1, 2, 3, 4, 5), uniform="uniform", weight=1)
+
         self.labelReadPreference = tk.Label(self, text="Read Preference")
-        self.labelReadPreference.grid()
+        self.labelReadPreference.grid(row=0, column=0)
 
-        self.buttonDefault = tk.Button(self, text="Default")
-        self.buttonDefault.grid()
+        self.buttonDefault = ttk.Button(self, text="Default")
+        self.buttonDefault.grid(row=1, column=0)
 
-        self.buttonPrimary = tk.Button(self, text="Primary")
-        self.buttonPrimary.grid()
+        self.buttonPrimary = ttk.Button(self, text="Primary")
+        self.buttonPrimary.grid(row=1, column=1)
 
-        self.buttonPrimaryPreferred = tk.Button(self, text="Primary Preferred")
-        self.buttonPrimaryPreferred.grid()
+        self.buttonPrimaryPreferred = ttk.Button(self, text="Primary\nPreferred")
+        self.buttonPrimaryPreferred.grid(row=1, column=2)
 
-        self.buttonSecondary = tk.Button(self, text="Secondary")
-        self.buttonSecondary.grid()
+        self.buttonSecondary = ttk.Button(self, text="Secondary")
+        self.buttonSecondary.grid(row=1, column=3)
 
-        self.buttonSecondaryPreferred = tk.Button(self, text="Secondary Preferred")
-        self.buttonSecondaryPreferred.grid()
+        self.buttonSecondaryPreferred = ttk.Button(self, text="Secondary\nPreferred")
+        self.buttonSecondaryPreferred.grid(row=1, column=4)
 
-        self.buttonNearest = tk.Button(self, text="Nearest")
-        self.buttonNearest.grid()
+        self.buttonNearest = ttk.Button(self, text="Nearest")
+        self.buttonNearest.grid(row=1, column=5)
+
+        self.labelReplicaSetName = ttk.Label(self, text="Replica Set Name")
+        self.labelReplicaSetName.grid(row=3, columnspan=3)
+
+        self.entryReplicaSetName = ttk.Entry(self)
+        self.entryReplicaSetName.grid(row=4, columnspan=3)
+
+        self.labelDefaultAuthenticationDatabase = ttk.Label(self, text="Default Authentication Database")
+        self.labelDefaultAuthenticationDatabase.grid(row=5, columnspan=3)
+
+        self.entryDefaultAuthenticationDatabase = ttk.Entry(self)
+        self.entryDefaultAuthenticationDatabase.grid(row=6, columnspan=3)
