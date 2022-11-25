3467aa4c9f5cf4e8e5eced260f61819e035521e9
Andres6936 <joan.buritica@artemisa.tech>
2022-11-24 19:58:58 -0500

    Use of typing

--- source/interfaz/PanelGeneral.py
+++ source/interfaz/PanelGeneral.py
@@ -11,8 +11,10 @@ class PanelGeneral(tk.Frame):
         self.setPort = setPort
         self.setHostname = setHostname
 
+        self.grid_columnconfigure((0, 1), uniform="uniform", weight=1)
+
         self.connectionStringScheme = tk.Label(self, text="Connection String Scheme")
-        self.connectionStringScheme.grid(row=0, column=0, sticky=tk.W)
+        self.connectionStringScheme.grid(row=0, column=0, columnspan=3, sticky=tk.W)
 
         self.buttonConnectionMongo = ttk.Button(self, text="mongodb")
         self.buttonConnectionMongo.grid(row=1, column=0, sticky=tk.W + tk.E)
@@ -26,22 +28,22 @@ class PanelGeneral(tk.Frame):
                                                      "to a MongoDB deployment: standalone,"
                                                      " replica set, or a sharded cluster.",
                                           wraplength="400px")
-        self.buttonDescription.grid(row=2, column=0, columnspan=2, sticky=tk.W)
+        self.buttonDescription.grid(row=2, column=0, columnspan=3, sticky=tk.W)
 
         self.labelHostname = tk.Label(self, text="Host")
-        self.labelHostname.grid(row=3, column=0, columnspan=2, sticky=tk.W)
+        self.labelHostname.grid(row=3, column=0, columnspan=3, sticky=tk.W)
 
         self.hostnameVar = tk.StringVar(value=f"{hostname.get()}:{port.get()}")
         self.hostnameVar.trace_add(mode="write", callback=self.updateHostnamePort)
         self.inputHostname = ttk.Entry(self, textvariable=self.hostnameVar)
-        self.inputHostname.grid(row=4, column=0, columnspan=2, sticky=tk.W + tk.E)
+        self.inputHostname.grid(row=4, column=0, columnspan=3, sticky=tk.W + tk.E)
 
         self.checkDirectConnection = ttk.Checkbutton(self, text="Direct Connection")
-        self.checkDirectConnection.grid(row=5, column=0, sticky=tk.W)
+        self.checkDirectConnection.grid(row=5, column=0, columnspan=3, sticky=tk.W)
 
         self.labelDescriptionDirectConnection = tk.Label(self,
                                                          text="Specifies whether to force dispatch all operations to the specified host.")
-        self.labelDescriptionDirectConnection.grid(row=6, column=0, sticky=tk.W)
+        self.labelDescriptionDirectConnection.grid(row=6, column=0, columnspan=3, sticky=tk.W)
 
     def updateHostnamePort(self, *args):
         (hostname, port) = self.hostnameVar.get().split(":")
