6a70c68ef3e9d94ea004cbed1ee460f9142f521b
Andres6936 <joan.buritica@artemisa.tech>
2022-11-26 15:06:03 -0500

    Prepare Mockups User

--- source/user/PanelListUsers.py
+++ source/user/PanelListUsers.py
@@ -1,12 +1,33 @@
 import tkinter as tk
+from tkinter import ttk
 
 from source.user.User import User
 
 
 class PanelListUsers(tk.Frame):
+    """
+    Ref: https://blog.teclado.com/tkinter-scrollable-frames/
+    """
+
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
-        for i in range(0, 5):
-            self.user = User(self)
-            self.user.grid(row=i, column=0)
+        canvas = tk.Canvas(self)
+        scrollbar = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
+        self.scrollableFrame = ttk.Frame(canvas)
+        self.scrollableFrame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(
+                scrollregion=canvas.bbox("all")
+            )
+        )
+
+        canvas.create_window((0, 0), window=self.scrollableFrame, anchor="nw")
+        canvas.configure(yscrollcommand=scrollbar.set)
+        canvas.pack(side="left", fill="both", expand=True)
+
+        scrollbar.pack(side="right", fill="y")
+
+        for i in range(0, 25):
+            self.user = User(self.scrollableFrame)
+            self.user.pack()
--- source/user/PanelUser.py
+++ source/user/PanelUser.py
@@ -8,10 +8,10 @@ class PanelUser(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
-        self.grid_columnconfigure((0, 1), uniform="uniform", weight=1)
+        self.grid_columnconfigure((0, 1), weight=1)
 
         self.panelUserList = PanelListUsers(self)
-        self.panelUserList.grid(row=0, column=0)
+        self.panelUserList.grid(row=0, column=0, sticky="nsew")
 
         self.panelOptions = PanelOptions(self)
-        self.panelOptions.grid(row=0, column=1, sticky=tk.E + tk.N)
+        self.panelOptions.grid(row=0, column=1, sticky="nsew")
