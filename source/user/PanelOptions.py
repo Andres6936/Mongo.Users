e843606720b3695ad780452aae6cf26251f0751c
Andres6936 <joan.buritica@artemisa.tech>
2022-11-26 20:32:08 -0500

    Prepare Mockups User

--- source/user/PanelOptions.py
+++ source/user/PanelOptions.py
@@ -9,11 +9,26 @@ class PanelOptions(tk.Frame):
         self.config(padx=20, pady=20)
         self.grid_columnconfigure((0), uniform="uniform", weight=1)
 
+        self.labelUserManager = ttk.Label(self, text="User Manager")
+        self.labelUserManager.grid(row=0, sticky=tk.W)
+
         self.buttonCreateUser = ttk.Button(self, text="Create User")
-        self.buttonCreateUser.grid(row=0, sticky=tk.W + tk.E)
+        self.buttonCreateUser.grid(row=1, sticky=tk.W + tk.E)
 
         self.buttonUpdateUser = ttk.Button(self, text="Update User")
-        self.buttonUpdateUser.grid(row=1, sticky=tk.W + tk.E)
+        self.buttonUpdateUser.grid(row=2, sticky=tk.W + tk.E)
 
         self.buttonRemoveUser = ttk.Button(self, text="Remove User")
-        self.buttonRemoveUser.grid(row=2, sticky=tk.W + tk.E)
+        self.buttonRemoveUser.grid(row=3, sticky=tk.W + tk.E)
+
+        self.labelRolManager = ttk.Label(self, text="Rol Manager")
+        self.labelRolManager.grid(row=4, sticky=tk.W)
+
+        self.buttonCreateRol = ttk.Button(self, text="Create Rol")
+        self.buttonCreateRol.grid(row=5, sticky=tk.W + tk.E)
+
+        self.buttonUpdateRol = ttk.Button(self, text="Update Rol")
+        self.buttonUpdateRol.grid(row=6, sticky=tk.W + tk.E)
+
+        self.buttonRemoveRol = ttk.Button(self, text="Remove Rol")
+        self.buttonRemoveRol.grid(row=7, sticky=tk.W + tk.E)
--- source/user/User.py
+++ source/user/User.py
@@ -12,11 +12,17 @@ class User(tk.Frame):
         self.labelUsername = ttk.Label(self, text="Rol")
         self.labelUsername.grid(row=0, column=2, sticky=tk.E)
 
+        self.labelDatabase = ttk.Label(self, text="Database")
+        self.labelDatabase.grid(row=1, sticky=tk.W)
+
+        self.labelUserId = ttk.Label(self, text="UserID")
+        self.labelUserId.grid(row=2, sticky=tk.W)
+
         self.buttonShowDetails = ttk.Button(self, text="Show Details")
-        self.buttonShowDetails.grid(row=1, column=0)
+        self.buttonShowDetails.grid(row=3, column=0)
 
-        self.buttonUpdate = ttk.Button(self, text="Update")
-        self.buttonUpdate.grid(row=1, column=1)
+        self.buttonUpdate = ttk.Button(self, text="Update Rol")
+        self.buttonUpdate.grid(row=3, column=1)
 
-        self.buttonRemove = ttk.Button(self, text="Remove")
-        self.buttonRemove.grid(row=1, column=2)
+        self.buttonRemove = ttk.Button(self, text="Remove User")
+        self.buttonRemove.grid(row=3, column=2)
