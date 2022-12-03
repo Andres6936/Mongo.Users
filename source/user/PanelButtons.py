cfc5a9425f330906f6922662727671427505d3c4
Andres6936 <joan.buritica@artemisa.tech>
2022-12-03 14:21:40 -0500

    Added Button

--- source/user/PanelButtons.py
+++ source/user/PanelButtons.py
@@ -9,12 +9,15 @@ class PanelButtons(tk.Frame):
         tk.Frame.__init__(self, parent)
 
         self.config(pady=4, padx=4)
-        self.grid_columnconfigure(0, uniform="uniform", weight=1)
+        self.grid_columnconfigure((0, 1), uniform="uniform", weight=1)
 
         self.controller = controller
 
+        self.buttonApplyFilter = ttk.Button(self, text="Apply Filter")
+        self.buttonApplyFilter.grid(row=0, column=0, sticky=tk.W)
+
         self.buttonDisconnect = ttk.Button(self, text="Disconnect", command=self.disconnect)
-        self.buttonDisconnect.grid(sticky=tk.E)
+        self.buttonDisconnect.grid(row=0, column=1, sticky=tk.E)
 
     def disconnect(self):
         self.controller.showScene(TypeScene.PANEL_CONNECTION)
